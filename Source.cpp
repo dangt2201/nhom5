552e04539e33ea63b7416253adec9b8b8b5e76d5
Thanh <dh52201452@student.stu.edu.vn>
2025-11-04 08:39:48 +0700

    ht

--- Source.cpp
+++ Source.cpp
@@ -9,6 +9,21 @@ int tong(int a, int b)
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+bool kiemTraSoHoanThien(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    int tong_uoc = 1; 
+    for (int i = 2; i <= sqrt(n); ++i) {
+        if (n % i == 0) { 
+            tong_uoc += i; 
+            if (i * i != n) {
+                tong_uoc += n / i;
+            }
+        }
+    }
+    return tong_uoc == n;
+}
 int main()
 {	int a,b ;
     a = 10;
