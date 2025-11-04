f7ce7aaf7ce41777ab97a53bc49dc3ff5b5ca5f2
Son <dh52201372@student.stu.edu.vn>
2025-11-04 08:23:54 +0700

    da lam ham hieu

--- Source.cpp
+++ Source.cpp
@@ -6,7 +6,10 @@ int tong(int a, int b)
 	int sum = a + b;
 	return sum;
 }
-int hieu(int a, int b);
+int hieu(int a, int b){
+	int h = a - b;
+	return h;
+}
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
@@ -15,6 +18,7 @@ int main()
 	b = 20;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout<<"Tong =" << tong(a,b);
+	cout<<"Hieu =" << hieu(a,b);
 	system("pause");
 	return 0;
 }
