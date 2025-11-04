5be48733d44433bb469cc2ced439c5b9a1fa2de2
Thanh <dh52201452@student.stu.edu.vn>
2025-11-04 08:17:21 +0700

    da lam ham tong

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
+int tong(int a, int b)
+{
+	int sum = a + b;
+	return sum;
+}
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
-{
+{	int a,b ;
+    a = 10;
+	b = 20;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-	cout<<"Tong =";
+	cout<<"Tong =" << tong(a,b);
 	system("pause");
 	return 0;
 }
